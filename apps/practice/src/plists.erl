a2b6a54672dee252c5874e4ad3ff15ede6f90845
aaronlelevier <aaron.lelevier@gmail.com>
2021-04-28 06:59:21 -0700

    first commit

---
+++ apps/practice/src/plists.erl
@@ -0,0 +1,31 @@
+%%%-------------------------------------------------------------------
+%%% @author Aaron Lelevier
+%%% @doc Practice [lists](https://erlang.org/doc/man/lists.html)
+%%% @end
+%%% Created : 28. Apr 2021 6:41 AM
+%%%-------------------------------------------------------------------
+-module(plists).
+-author("Aaron Lelevier").
+-vsn(1.0).
+-export([]).
+-compile(export_all).
+
+any(L) ->
+  lists:any(fun(X) -> X =:= true end, L).
+
+all(L) ->
+  lists:all(fun(X) -> X =:= true end, L).
+
+droplast([]) -> [];
+droplast(L) ->
+  [_|T] = lists:reverse(L),
+  lists:reverse(T).
+
+%% Returns a reusable 'dropwhile' for a Predicate that needs to be
+%% applied to multiple lists
+dropwhile(Pred) ->
+  fun(L) -> lists:dropwhile(Pred, L) end.
+
+%% Returns a string of 'L' items joined by 'Sep'
+join(Sep, L) ->
+  lists:concat(lists:join(Sep, L)).
